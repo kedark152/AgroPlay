fb619bde2aa205c6470af09b8515f6b3d0bbba3f
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-04-28 15:58:01 +0530

    feat: added videos data

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,93 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+    _id: "9KGukRIIyr",
+    title: "Jugaad vs. High Budget Farming | Indian Farmer",
+    creator: "Indian Farmer",
+    profileUrl:
+      "https://yt3.ggpht.com/ytc/AKedOLT6jpVi2qs0FvKHGtIIgoPNownbynnvHSmt-RyMug=s88-c-k-c0x00ffffff-no-rj",
+    views: "74461",
+    uploadDate: "Sep 28, 2021",
+    videoLength: "07:38",
+    thumbnailUrl: "http://i3.ytimg.com/vi/SHC7cisa4ds/hq720.jpg",
+    thubmnailTitle: "High Budget vs. Low Budget Farming",
+    videoUrl: "https://youtu.be/SHC7cisa4ds",
+    description: "High Budget vs. Low Budget Farming | Indian Farmer",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "5PI1a5E3Ql",
+    title:
+      "Mango Farming Video  | ऐसे स्मार्ट किसानों के वजह से ही खेती टिकी हुई हैं  ",
+    creator: "Indian Farmer",
+    profileUrl:
+      "https://yt3.ggpht.com/ytc/AKedOLT6jpVi2qs0FvKHGtIIgoPNownbynnvHSmt-RyMug=s88-c-k-c0x00ffffff-no-rj",
+    views: "632595",
+    uploadDate: "Jun 1, 2021",
+    videoLength: "19:52",
+    thumbnailUrl: "http://i3.ytimg.com/vi/Tf57e9yjtZQ/hq720.jpg",
+    thubmnailTitle: "Mango Farming Techniques",
+    videoUrl: "https://youtu.be/Tf57e9yjtZQ",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "COMMERCIAL MANGO FARMING PROFIT & MARKETING | Mango Farming Techniques ",
+  },
+  {
+    _id: "NDLkd8b7yR",
+    title:
+      "Tomato farming | टमाटर की खेती से कमाये लाखो रूपये | टमाटर की खेती | Full Video | A2Z जानकारी",
+    creator: "The Advance Agriculture",
+    profileUrl:
+      "https://yt3.ggpht.com/ytc/AKedOLS5gWmZ5oQtIzXBbWws5TMz8H-xJqKm6BLCZtoQmA=s88-c-k-c0x00ffffff-no-rj",
+    views: "2724892",
+    uploadDate: "Nov 10, 2020",
+    videoLength: "45:39",
+    thumbnailUrl: "http://i3.ytimg.com/vi/IFZqx2KftS4/hq720.jpg",
+    thubmnailTitle: "Detailed Tomato Farming Technique",
+    videoUrl: "https://youtu.be/IFZqx2KftS4",
+    description: "Detailed Tomato Farming Technique ",
+  },
+  {
+    _id: "DmlC9SSM4E",
+    title:
+      "Profitable Potato farming 🥔 Seed to harvest | आलू की खेती की पूरी जानकारी",
+    creator: "Organic Acre",
+    profileUrl:
+      "https://yt3.ggpht.com/14HO-rzYU6i9Fdx-GfAcPdhmWTyC2TKJk-9QJhKDm5DalB1HE_O4TeEe3u0HnpkhaqxWsMckLA=s88-c-k-c0x00ffffff-no-rj",
+    views: "26382",
+    uploadDate: "Jan 24, 2022",
+    videoLength: "14:40",
+    thumbnailUrl: "http://i3.ytimg.com/vi/LW5FCxJkFR0/hq720.jpg",
+    thubmnailTitle: "Detailed Potato Farming Technique",
+    videoUrl: "https://youtu.be/LW5FCxJkFR0",
+    description: "Detailed Potato Farming Technique ",
+  },
+  {
+    _id: "Z2UnZdcXYu",
+    title:
+      "How to control Thrips & whitefly in Watermelon & Musk Melon Crop | Solomon |Alika |Bio R-303 |tarbuj",
+    creator: "The Advance Agriculture",
+    profileUrl:
+      "https://yt3.ggpht.com/ytc/AKedOLS5gWmZ5oQtIzXBbWws5TMz8H-xJqKm6BLCZtoQmA=s88-c-k-c0x00ffffff-no-rj",
+    views: "15952",
+    uploadDate: "Mar 15, 2022",
+    videoLength: "05:04",
+    thumbnailUrl: "http://i3.ytimg.com/vi/Ht7YsLEBfpo/hq720.jpg",
+    thubmnailTitle: "control Thrips & whitefly in Watermelon & Musk Melon Crop",
+    videoUrl: "https://youtu.be/Ht7YsLEBfpo",
+    description: "control Thrips & whitefly in Watermelon & Musk Melon Crop ",
+  },
+  {
+    _id: "EKcLLHgDhb",
+    title:
+      " Israel Technique Avocado Fruit Farming 1200रू Kg बिकने वाले एवोकाडो से कमाये लाखों इजराइल तकनीक से",
+    creator: "Farming Leader",
+    profileUrl:
+      "https://yt3.ggpht.com/kob9_5rTR2E50ah1gynPuJar9wKLz83KvxNFYHMX0QgVgTQbns75egv0fqxQko3FiHb7jUHF9g=s88-c-k-c0x00ffffff-no-rj",
+    views: "571389",
+    uploadDate: "Mar 27, 2022",
+    videoLength: "17:15",
+    thumbnailUrl: "http://i3.ytimg.com/vi/chEgEGnbaK8/hq720.jpg",
+    thubmnailTitle: "Israel Technique Avocado Fruit Farming",
+    videoUrl: "https://youtu.be/Ht7YsLEBfpo",
+    description: "Israel Technique Avocado Fruit Farming",
   },
 ];
