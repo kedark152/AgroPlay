805bdcf42f28d436dd53fc5c0ec111a400bc237f
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-04-28 16:02:25 +0530

    feat: created chips container

---
+++ src/components/ChipsContainer.js
@@ -0,0 +1,15 @@
+import "../styles/components/chipsContainer.css";
+
+export const ChipsContainer = () => {
+  return (
+    <>
+      <div className="chips fw-bold pd-y-xsm">
+        <div id="basic-chip">All</div>
+        <div id="basic-chip">Tomato farming</div>
+        <div id="basic-chip">Pesticides</div>
+        <div id="basic-chip">Organic Fertilizers</div>
+        <div id="basic-chip">Indian Farmer</div>
+      </div>
+    </>
+  );
+};
