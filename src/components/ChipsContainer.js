6a596c06a21dfc73a0c81f1f74f02f129b34c3a8
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-04-29 21:10:18 +0530

    feat: made chip container active

--- src/components/ChipsContainer.js
+++ src/components/ChipsContainer.js
@@ -1,13 +1,36 @@
 import "../styles/components/chipsContainer.css";
+import { useState } from "react";
 
-export const ChipsContainer = () => {
+export const ChipsContainer = ({ getActiveChip }) => {
+  const keyWordsList = [
+    { id: 1, keyName: "All" },
+    { id: 2, keyName: "Vegetable Farming" },
+    { id: 3, keyName: "Pesticides" },
+    { id: 4, keyName: "Fruit Farming" },
+    { id: 5, keyName: "Indian Farmer" },
+  ];
+
+  const [isActiveChip, setIsActiveChip] = useState(0);
+  const setChipStyle = (index) => {
+    if (index === isActiveChip) return "active-chip";
+    else return "inactive-chip";
+  };
+
   return (
     <div className="chips fw-bold pd-y-xsm">
-      <div id="basic-chip">All</div>
-      <div id="basic-chip">Tomato farming</div>
-      <div id="basic-chip">Pesticides</div>
-      <div id="basic-chip">Organic Fertilizers</div>
-      <div id="basic-chip">Indian Farmer</div>
+      {keyWordsList.map((item, index) => (
+        <div
+          key={item.id}
+          id={index}
+          className={`basic-chip ${setChipStyle(index)}`}
+          onClick={() => {
+            setIsActiveChip(index);
+            getActiveChip(item.keyName);
+          }}
+        >
+          {item.keyName}
+        </div>
+      ))}
     </div>
   );
 };
