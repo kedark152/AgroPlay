5a4e38dfd5609f850498537e94545dc61cc4dd1c
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-05-02 22:23:17 +0530

    added auth page routes

--- src/routes/PageRoutes.js
+++ src/routes/PageRoutes.js
@@ -4,6 +4,8 @@ import { Explore } from "../pages/Explore";
 import { History } from "../pages/History";
 import { Home } from "../pages/Home";
 import { LikedVideos } from "../pages/LikedVideos";
+import { Login } from "../pages/Login";
+import { Signup } from "../pages/Signup";
 import { MyProfile } from "../pages/MyProfile";
 import { Playlist } from "../pages/Playlist";
 import { WatchLater } from "../pages/WatchLater";
@@ -18,6 +20,8 @@ export const PageRoutes = () => {
       <Route path="/history" element={<History />} />
       <Route path="/watchlater" element={<WatchLater />} />
       <Route path="/profile" element={<MyProfile />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/mock" element={<Mockman />} />
     </Routes>
   );
