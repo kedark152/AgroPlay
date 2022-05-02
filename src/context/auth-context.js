2dbfef6313cc81bb7f1ad84b1634a50a3d96e1da
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-05-02 22:23:57 +0530

    feat: created useAuth() hook

---
+++ src/context/auth-context.js
@@ -0,0 +1,25 @@
+import { useState, useContext, createContext, useEffect } from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({});
+  useEffect(() => {
+    (async () => {
+      const token = localStorage.getItem("token");
+      token
+        ? setAuth({ token: token, isLoggedIn: true })
+        : setAuth({ token: "", isLoggedIn: false });
+    })();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => useContext(AuthContext);
+
+export { AuthProvider, useAuth };
