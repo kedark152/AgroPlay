ba954c7745074db9abd779d7bd7b42c0d12795ba
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-05-08 23:30:55 +0530

    feat: updated action buttons with respective functions to call

--- src/components/VideoOptions.js
+++ src/components/VideoOptions.js
@@ -1,22 +1,48 @@
 import "../styles/components/videoOptions.css";
-import { usePlaylist } from "../context/playlist-context";
+import { useVideoAction } from "../context/video-action-context";
 import { RequiresAuth } from "../routes/RequiresAuth";
 import { useAuth } from "../context/auth-context";
-export const VideoOptions = ({ videoId }) => {
-  const { dispatchPlaylist } = usePlaylist();
+import { addToWatchLater } from "../utils/addToWatchLater";
+import { removeFromWatchLater } from "../utils/removeFromWatchLater";
+import { addToLikes } from "../utils/addToLikes";
+import { removeFromLikes } from "../utils/removeFromLikes";
+
+export const VideoOptions = ({ activeVideo }) => {
+  const { videoActionState, dispatchVideoAction } = useVideoAction();
   const { auth } = useAuth();
+
+  const isInWatchLaterList = (videoId) => {
+    return videoActionState.watchlater.some((item) => item._id === videoId);
+  };
+  const isInLikesList = (videoId) => {
+    return videoActionState.likes.some((item) => item._id === videoId);
+  };
+
   return (
     <div className="video-options-box flex-column">
       <RequiresAuth>
         <ul>
-          <li>
-            <i className="material-icons">watch_later</i>Save to watch later
+          <li
+            onClick={() => {
+              isInWatchLaterList(activeVideo._id)
+                ? removeFromWatchLater({
+                    auth,
+                    activeVideo,
+                    dispatchVideoAction,
+                  })
+                : addToWatchLater({ auth, activeVideo, dispatchVideoAction });
+            }}
+          >
+            <i className="material-icons">watch_later</i>
+            {isInWatchLaterList(activeVideo._id)
+              ? `Remove from watch later`
+              : `Save to watch later`}
           </li>
           <li
             onClick={() =>
-              dispatchPlaylist({
+              dispatchVideoAction({
                 type: "TOGGLE-PLAYLIST-BOX",
-                payload: videoId,
+                payload: activeVideo,
                 token: auth.token,
               })
             }
@@ -24,8 +50,21 @@ export const VideoOptions = ({ videoId }) => {
             <i className="material-icons">playlist_add</i>
             Save to playlist
           </li>
-          <li>
-            <i className="material-icons">thumb_up</i>Add to liked videos
+          <li
+            onClick={() => {
+              isInLikesList(activeVideo._id)
+                ? removeFromLikes({
+                    auth,
+                    activeVideo,
+                    dispatchVideoAction,
+                  })
+                : addToLikes({ auth, activeVideo, dispatchVideoAction });
+            }}
+          >
+            <i className="material-icons">thumb_up</i>
+            {isInLikesList(activeVideo._id)
+              ? `Remove from liked videos`
+              : `Add to liked videos`}
           </li>
         </ul>
       </RequiresAuth>
