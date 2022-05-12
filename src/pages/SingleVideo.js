a014a97d3335698133c148b75aa16ad597984323
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-05-12 18:35:24 +0530

    feat: updated video link for autoplay

--- src/pages/SingleVideo.js
+++ src/pages/SingleVideo.js
@@ -117,7 +117,7 @@ export const SingleVideo = () => {
               <div className="video-responsive">
                 <iframe
                   className="youTube-iframe"
-                  src={`https://www.youtube.com/embed/${video._id}`}
+                  src={`https://www.youtube.com/embed/${video._id}?autoplay=1`}
                   title="YouTube video player"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
